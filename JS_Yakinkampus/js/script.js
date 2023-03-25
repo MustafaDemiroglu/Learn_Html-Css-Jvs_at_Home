f6eda134d6108ff2656f9bf88863326563aa6eb6
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-25 23:40:03 +0100

    js 1. ders

--- JS_Yakinkampus/js/script.js
+++ JS_Yakinkampus/js/script.js
@@ -2,3 +2,7 @@ var yas = 1 ;
 var yil = 5 ;
 document.write ("Yakin Kampüs Sitesi "+yas+" yasinda. "+ yil +" sonra "+(yil+yas)+" yasinda olacak")
 // document.write(" Yandaki yazinin dogrulugundan emin degilim")
+/*
+buray ne yazarsan yaz hic bir etkisi olmaz
+
+*/
