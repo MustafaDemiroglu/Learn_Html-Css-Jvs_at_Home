aa24fd0da20621e866d9e4ab2277b01a8ac6dc77
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-25 23:38:08 +0100

    Java scripte basladim

---
+++ JS_Yakinkampus/js/script.js
@@ -0,0 +1,4 @@
+var yas = 1 ;
+var yil = 5 ;
+document.write ("Yakin Kampüs Sitesi "+yas+" yasinda. "+ yil +" sonra "+(yil+yas)+" yasinda olacak")
+// document.write(" Yandaki yazinin dogrulugundan emin degilim")
