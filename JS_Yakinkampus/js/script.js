2261957b0ba8323725ad59db6074fcc82b3fea12
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-26 13:52:26 +0200

    js 6

--- JS_Yakinkampus/js/script.js
+++ JS_Yakinkampus/js/script.js
@@ -6,9 +6,21 @@ function yas(x) {
 yas(29);
 */
 
+/* 
 function kimlik(ad,soyad) {
     document.write("Merhaba benim adim:"+<strong>ad</strong>+"Soyadim:"+<strong>soyad</strong>);
 }
 
 kimlik("Mesut","Gün");
 kimlik("Mustafa","Demiroglu");
+*/
+
+function deneme(x) {
+    return x*x;
+}
+document.write (deneme(5));
+
+function deneme2(y) {
+    alert(y*y*y);
+}
+deneme2(deneme(4));
