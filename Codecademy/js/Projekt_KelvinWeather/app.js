e6937bc7ced407d4aa7c07f1afb801406c30c8ac
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-12-17 21:47:54 +0100

    update

--- Codecademy/js/Projekt_KelvinWeather/app.js
+++ Codecademy/js/Projekt_KelvinWeather/app.js
@@ -1,6 +1,9 @@
-// to declare a variable 
+// to declare a variable for Kelvin
 var kelvin = 293; 
 var celcius;
 // convert Kelvin to Celsius by subtracting 273 from the kelvin variable
 celcius = kelvin - 273;
-// morgen weiter
+// convert celcius to fahrenheit and round the number saved to fahrenheit
+var fahrenheit = Math.ceil(celcius * (9/5)+32);
+
+console.log(`The temperature is $`fahrenheit` degrees Fahrenheit`);
