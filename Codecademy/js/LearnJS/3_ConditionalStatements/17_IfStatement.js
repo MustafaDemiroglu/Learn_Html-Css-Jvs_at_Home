30847133ee459f17af1fea550b70572e1d40ecaa
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-12-18 20:58:15 +0100

    if statement

---
+++ Codecademy/js/LearnJS/3_ConditionalStatements/17_IfStatement.js
@@ -0,0 +1,10 @@
+if (true) {
+    console.log('This message will print!'); 
+  }
+  // Prints: This message will print!
+
+  let sale  = true ;
+  // sale = false;
+  if (sale) {
+    console.log('Time to buy!'); 
+  }  
