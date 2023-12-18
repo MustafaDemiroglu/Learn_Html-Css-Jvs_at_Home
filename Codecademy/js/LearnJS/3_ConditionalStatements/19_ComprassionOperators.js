53d615ed2d62899e0ed0bb0b09cfeb7f07d5afac
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-12-18 21:05:46 +0100

    i else

---
+++ Codecademy/js/LearnJS/3_ConditionalStatements/19_ComprassionOperators.js
@@ -0,0 +1,18 @@
+/*
+Less than: <
+Greater than: >
+Less than or equal to: <=
+Greater than or equal to: >=
+Is equal to: ===
+Is not equal to: !==
+10 < 12 // Evaluates to true
+'apples' === 'oranges' // false
+*/
+
+let hungerLevel = 7;
+if(hungerLevel > 7) {
+    console.log('Time to eat!');
+  } else {
+    console.log('We can eat later!');
+  }
+    
