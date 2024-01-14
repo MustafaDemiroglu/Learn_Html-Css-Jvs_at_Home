5698add048eb6c7a4483090ccea0dca60d28a2e7
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-01-14 20:44:06 +0100

    Update main.js

--- Codecademy/js/Projekt_MagicEightBall/main.js
+++ Codecademy/js/Projekt_MagicEightBall/main.js
@@ -6,3 +6,26 @@ var userQuestion;
 console.log('Wie kan Ihnen behilflich sein? ' + userName);
 
 var randomNumber = Math.floor(Math.random() * 8) ;
+console.log('random number : ' + randomNumber);
+var eightBall;
+
+if (randomNumber == 0) {
+  eightBall = 'It is certain';
+} else if (randomNumber == 1) {
+  eightBall = 'It is decidedly so';
+} else if (randomNumber == 2) {
+  eightBall = 'Reply hazy try again';
+} else if (randomNumber == 3) {
+  eightBall = 'Cannot predict now';
+} else if (randomNumber == 4) {
+  eightBall = 'Do not count on it';
+} else if (randomNumber == 5) {
+  eightBall = 'My sources say no';
+} else if (randomNumber == 6) {
+  eightBall = 'Outlook not so good';
+} else if (randomNumber == 7) {
+  eightBall = 'Please not again';
+} else if (randomNumber == 8) {
+  eightBall = 'Signs point to yes';
+}
+console.log(eightBall);
