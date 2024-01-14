6229c1348176317408baab60c4f7589878add381
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-01-14 20:26:48 +0100

    Update main.js

--- Codecademy/js/Projekt_MagicEightBall/main.js
+++ Codecademy/js/Projekt_MagicEightBall/main.js
@@ -4,3 +4,5 @@ userName ? console.log('Hello, '+ userName) : console.log('Hello!');
 
 var userQuestion;
 console.log('Wie kan Ihnen behilflich sein? ' + userName);
+
+var randomNumber = Math.floor(Math.random() * 8) ;
