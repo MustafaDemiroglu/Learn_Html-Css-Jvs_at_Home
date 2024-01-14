c2645f3401f77570e53f28c4c8b2dce6ec530382
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-01-14 21:16:47 +0100

    Update main.js

--- Codecademy/js/Projekt_RaceDay/main.js
+++ Codecademy/js/Projekt_RaceDay/main.js
@@ -1,6 +1,19 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
-var isEarly = true;
+var isEarly = false;
 
-var Age = 18;
+var age = 17;
 
+if (age >= 18 && isEarly ) {
+     raceNumber += 1000;
+  }  
+
+if(age >= 18 && isEarly) {
+     console.log("Your race number is " +raceNumber +' and you will race at 09.30 am.');
+} else if (age >= 18 && !isEarly) {
+      console.log("Your race number is " +raceNumber +' and you will race at 11.00 am.'); 
+} else if (age < 18) {
+      console.log("Your race number is " + raceNumber +' and you will race at 12.30 pm.'); 
+} else {
+      console.log("You  should see the registration desk"); 
+}
