dde28111ac373989251cbd085cdbd44fe8fe7b78
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-01-31 22:15:25 +0100

    djhfkl

--- Codecademy/js/Projekt_RockPaperScissors/rockPaperScissors.js
+++ Codecademy/js/Projekt_RockPaperScissors/rockPaperScissors.js
@@ -9,4 +9,18 @@ const getUserChoice = (userInput) => {
     }
 };
 
+function getComputerChoice() {
+  let temp = Math.floor(0,2);
 
+  if(temp == 0) {
+      return "rock";
+    } else if (temp == 1)  {
+      return "paper";
+    } else if (temp == 2) {
+      return "scissors";
+    } else {
+      return "Kestane";
+    }
+}
+
+console.log(getComputerChoice());
