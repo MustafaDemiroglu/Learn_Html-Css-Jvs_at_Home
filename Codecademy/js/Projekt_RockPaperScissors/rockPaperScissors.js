ffeba8dc560a76dd55f4575f26469cce8bfa2ba7
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-01 19:26:33 +0100

    game is finished

--- Codecademy/js/Projekt_RockPaperScissors/rockPaperScissors.js
+++ Codecademy/js/Projekt_RockPaperScissors/rockPaperScissors.js
@@ -1,8 +1,9 @@
 console.log('Hello! Wellcome to our rock, paper or scissors Game! Fell free to enjoy !!!');
+console.log("");
 
 const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
-  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'|| userInput === 'bomb') {
       return userInput;
     } else {
       return "Error, Mistake";
@@ -23,31 +24,47 @@ function getComputerChoice() {
 }
 
 function determineWinner(userChoice, computerChoice) {
-  if(userChoice == computerChoice) {
-      return "tie";
+  if(userChoice == "bomb"){
+      return "Programmierer win always";
+  } else if (userChoice == "Error, Mistake"){
+      return "User has given an unacceptable choice. That is why, game is cancelled. Please try again later with a right choice ";
     } else {
-      if(userChoice == "rock") {
-        if(computerChoice == "paper") {
-          return " computer won";
-        } else {
-            return "user won";
+        if(userChoice == computerChoice) {
+        return "tie";
+      } else {
+        if(userChoice == "rock") {
+          if(computerChoice == "paper") {
+            return " computer won";
+          } else {
+              return "user won";
+          }
         }
-      }
-      if(userChoice == "paper") {
-        if(computerChoice == "scissors") {
-          return " computer won";
-        } else {
-            return "user won";
+        if(userChoice == "paper") {
+          if(computerChoice == "scissors") {
+            return " computer won";
+          } else {
+              return "user won";
+          }
         }
-      }
-      if(userChoice == "scissors") {
-        if(computerChoice == "rock") {
-          return " computer won";
-        } else {
-            return "user won";
+        if(userChoice == "scissors") {
+          if(computerChoice == "rock") {
+            return " computer won";
+          } else {
+              return "user won";
+          }
         }
-      }
     }
-
+  }
 }
 
+function playGame(){
+  let userChoice = getUserChoice("rock");
+  console.log("userChoice : " + userChoice)
+  let computerChoice = getComputerChoice();
+  console.log("computerChoice : " + computerChoice);
+  console.log("#####################");
+  console.log("Who has won the game : ")
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
