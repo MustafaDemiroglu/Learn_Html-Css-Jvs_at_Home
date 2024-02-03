ea837f4e1247a24ca5cdcc3c21beaecf17e05376
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-03 18:03:43 +0100

    Scope update

---
+++ Codecademy/js/LearnJS/5_Scope/34_BlocksAndScope.js
@@ -0,0 +1,16 @@
+const logSkyColor = () => {
+    let color = 'blue'; 
+    console.log(color); // blue 
+}
+
+if (dusk) {
+    let color = 'pink';
+    console.log(color); // pink
+}
+  
+const city = 'New York City';
+
+function logCitySkyline() {
+  let skyscraper = 'Empire State Building';
+     
+}
