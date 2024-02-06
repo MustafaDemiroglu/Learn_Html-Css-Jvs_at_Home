c63fcf246dce7ed1fc20da1b4215f78628238dae
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-06 21:18:24 +0100

    update

---
+++ Codecademy/js/LearnJS/6_Arrays/41_UpdateElements.js
@@ -0,0 +1,9 @@
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+
+seasons[3] = 'Autumn';
+console.log(seasons); 
+//Output: ['Winter', 'Spring', 'Summer', 'Autumn']
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados';
