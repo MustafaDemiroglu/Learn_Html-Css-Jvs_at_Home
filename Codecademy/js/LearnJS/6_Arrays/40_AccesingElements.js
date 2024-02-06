802dbb3fe0d484245f5f9c333cc8db8ecf27b2df
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-06 21:12:04 +0100

    Accessing Elements

---
+++ Codecademy/js/LearnJS/6_Arrays/40_AccesingElements.js
@@ -0,0 +1,12 @@
+const hello = 'Hello World';
+console.log(hello[6]);
+// Output: W
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+var listItem = famousSayings[0];
+console.log(listItem);  // Fortune favors the brave.
+
+console.log(famousSayings[2]);  // Where there is love there is life.
+
+console.log(famousSayings[3]);  // undefined
