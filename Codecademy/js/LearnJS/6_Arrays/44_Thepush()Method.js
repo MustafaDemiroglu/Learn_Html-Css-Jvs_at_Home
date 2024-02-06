2c609bf953c40208cb54683f7b9a885f831c8e44
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-06 21:36:41 +0100

    update

---
+++ Codecademy/js/LearnJS/6_Arrays/44_Thepush()Method.js
@@ -0,0 +1,10 @@
+const itemTracker = ['item 0', 'item 1', 'item 2'];
+
+itemTracker.push('item 3', 'item 4');
+
+console.log(itemTracker); 
+// Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+chores.push('go Home', 'Who cares');
+console.log(chores); // [ 'wash dishes','do laundry','take out trash','go Home','Who cares' ]
