c625ef1c7fe6400b8b5e23a76235dde1a4b8a6d8
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-06 21:03:34 +0100

    fertig

---
+++ Codecademy/js/LearnJS/6_Arrays/39_CreatAnArray.js
@@ -0,0 +1,4 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+const hobbies = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(hobbies);
