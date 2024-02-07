9699661034bdfd9556e3f0963c6735fa6f9564a8
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-07 22:25:19 +0100

    nested Arry

---
+++ Codecademy/js/LearnJS/6_Arrays/48_NestedArrays.js
@@ -0,0 +1,7 @@
+const nestedArr = [[1], [2, 3]];
+console.log(nestedArr[1]); // Output: [2, 3]
+console.log(nestedArr[1][0]); // Output: 2
+
+const numberClusters = [[1,2], [3, 4],[5, 6]];
+const target = numberClusters[2][1]; 
+console.log(target);    // 6
