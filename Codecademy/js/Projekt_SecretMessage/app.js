7bb43211f3c626ae043d5018ae054fac04e2d08d
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-10 18:50:35 +0100

    Update app.js (#2)
    Bei Hacer habe ich obengenannte Änderungen gemacht

--- Codecademy/js/Projekt_SecretMessage/app.js
+++ Codecademy/js/Projekt_SecretMessage/app.js
@@ -1,7 +1,20 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
 console.log(secretMessage.length);
-secretMessage.pop();
 
+secretMessage.pop();
 console.log(secretMessage.length);
 
+secretMessage.push("to", "Program");
+console.log(secretMessage.length);
+
+secretMessage[7] = 'right';
+
+secretMessage.shift();
+console.log(secretMessage.length);
+
+secretMessage.unshift('Programming');
+console.log(secretMessage.length);
+
+secretMessage.splice(6,5,'know');
+console.log(secretMessage.length);
