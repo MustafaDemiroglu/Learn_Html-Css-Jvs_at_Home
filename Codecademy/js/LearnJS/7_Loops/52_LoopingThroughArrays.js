68a1fd4f8489d91510e4ee31ae15095b8f20b82d
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-11 22:13:03 +0100

    array in Loop

---
+++ Codecademy/js/LearnJS/7_Loops/52_LoopingThroughArrays.js
@@ -0,0 +1,11 @@
+const animals = ['Grizzly Bear', 'Sloth', 'Sea Lion'];
+for (let i = 0; i < animals.length; i++){
+  console.log(animals[i]);
+}
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++){
+  console.log('I would love to visit ' + vacationSpots[i] );
+}
