6541c8620889b71e3ea06e5b42856a8935b85039
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-12 21:42:59 +0100

    resultarray

--- Codecademy/js/Projekt_WhaleTalk/main.js
+++ Codecademy/js/Projekt_WhaleTalk/main.js
@@ -2,5 +2,10 @@ let input = 'A Beautiful Home';
 const vowels = ['a', 'e', 'i','o','u'];
 let resultArray = [];
 for (let i = 0; i < input.length; i++) {
-    console.log(input.charAt(i));
+    for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j] ) {
+       resultArray.push(input[i]); 
+    }
 } 
+} 
+console.log(resultArray);
