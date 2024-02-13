7307e4d449cfe154c3488ee0a85ffe9b6269ab3a
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-13 20:52:18 +0100

    asad

--- Codecademy/js/LearnJS/8_Iterators/58_FunctionsAsData.js
+++ Codecademy/js/LearnJS/8_Iterators/58_FunctionsAsData.js
@@ -1,7 +1,18 @@
 const announceThatIAmDoingImportantWork = () => {
     console.log("I’m doing very important work!");
 };
-
 const busy = announceThatIAmDoingImportantWork;
-
 busy(); // This function call barely takes any space!
+
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+    for(let i = 1; i <= 1000000; i++) {
+      if ( (2 + 2) != 4) {
+        console.log('Something has gone very wrong :( ');
+      }
+    }
+  };
+  // Write your code below
+  const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+  isTwoPlusTwo();
+  console.log(isTwoPlusTwo.name);
+  
