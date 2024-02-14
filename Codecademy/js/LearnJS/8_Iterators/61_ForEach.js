b4b0359a469df89f50ee44c4da894fb81d5a6920
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-14 21:33:45 +0100

    foe each

---
+++ Codecademy/js/LearnJS/8_Iterators/61_ForEach.js
@@ -0,0 +1,11 @@
+groceries.forEach(groceryItem => console.log(groceryItem));
+function printGrocery(element){
+    console.log(element);
+}
+groceries.forEach(printGrocery);
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit => console.log("I want to eat a " + fruit));
+
