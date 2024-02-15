8db9a0db322286c872cb6c57992839f48ae6cb09
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-15 22:09:36 +0100

    update projekt

--- Codecademy/js/LearnJS/6_Arrays/41_UpdateElements.js
+++ Codecademy/js/LearnJS/6_Arrays/41_UpdateElements.js
@@ -1,9 +1,42 @@
-let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
 
-seasons[3] = 'Autumn';
-console.log(seasons); 
-//Output: ['Winter', 'Spring', 'Summer', 'Autumn']
+let storyWords = story.split(' ');
+let unnecessaryWord = 'literally';
+let misspelledWord = 'beautifull';
+let badWord = 'freaking';
 
-let groceryList = ['bread', 'tomatoes', 'milk'];
+let count = 0;
+storyWords.forEach(word => count++);
+console.log('1 - In this text there are ' + count + ' words.');
 
-groceryList[1] = 'avocados';
+let ohneUnnecessaryWord = storyWords.filter(word => {
+  return word !== unnecessaryWord;
+});
+let count2 = 0;
+ohneUnnecessaryWord.forEach(word => count2++);
+console.log('2 - In this text there are ' + count2 + ' words.');
+
+storyWords = ohneUnnecessaryWord.map(word =>{
+  if (word === misspelledWord) {
+      return 'beautiful';
+    } else {
+      return word;
+    }
+});
+let count3 = 0;
+storyWords.forEach(word => count3++);
+console.log('3 - In this text there are ' + count3 + ' words.');
+
+let badWordIndex = storyWords.findIndex(word => {
+  return word === badWord;
+});
+console.log(badWordIndex);
+
+storyWords[badWordIndex] = 'really';
+console.log(storyWords[badWordIndex]); 
+
+
+
+
+console.log (" ------ Last Control ------- ");
+console.log(storyWords.join(' '));
