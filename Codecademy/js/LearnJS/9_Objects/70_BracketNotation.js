966146af2ca8d15d696d489c1e7d986c4cd0672c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-17 23:22:36 +0100

    hello

---
+++ Codecademy/js/LearnJS/9_Objects/70_BracketNotation.js
@@ -0,0 +1,13 @@
+['A', 'B', 'C'][0]; // Returns 'A'
+
+let spaceship = {
+    'Fuel Type': 'Turbo Fuel',
+    'Active Duty': true,
+    homePlanet: 'Earth',
+    numCrew: 5
+};
+spaceship['Active Duty'];   // Returns true
+spaceship['Fuel Type'];   // Returns  'Turbo Fuel'
+spaceship['numCrew'];   // Returns 5
+spaceship['!!!!!!!!!!!!!!!'];   // Returns undefined
+
