79fbe20d3cf3f8387cebeb3106f05b2b87d9cd41
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-17 23:26:10 +0100

    bracket

--- Codecademy/js/LearnJS/9_Objects/70_BracketNotation.js
+++ Codecademy/js/LearnJS/9_Objects/70_BracketNotation.js
@@ -11,3 +11,21 @@ spaceship['Fuel Type'];   // Returns  'Turbo Fuel'
 spaceship['numCrew'];   // Returns 5
 spaceship['!!!!!!!!!!!!!!!'];   // Returns undefined
 
+let returnAnyProp = (objectName, propName) => objectName[propName];
+ 
+returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
+
+let spaceship2 = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
+  
+  // Write your code below
+  let isActive = spaceship2['Active Mission'];
+  
+  console.log (isActive);
+  console.log(propName);
