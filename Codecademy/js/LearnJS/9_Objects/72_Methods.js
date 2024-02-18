1da1271a958d4655ecc7e5d9e1929abed27d52d9
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-18 22:29:03 +0100

    ghj

---
+++ Codecademy/js/LearnJS/9_Objects/72_Methods.js
@@ -0,0 +1,14 @@
+const alienShip = {
+    invade: function () { 
+      console.log('Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.')
+    }
+};
+
+const alienShip2 = {
+    invade2 () { 
+      console.log('Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.')
+    }
+};
+
+alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+
