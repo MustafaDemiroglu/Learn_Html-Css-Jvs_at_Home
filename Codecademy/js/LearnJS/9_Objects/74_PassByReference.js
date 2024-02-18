bfd9be9fa9d8b6a08c4014a439b5b9617262a3dd
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-18 22:58:13 +0100

    reference

--- Codecademy/js/LearnJS/9_Objects/74_PassByReference.js
+++ Codecademy/js/LearnJS/9_Objects/74_PassByReference.js
@@ -4,6 +4,42 @@ const spaceship = {
 }; 
 let paintIt = obj => {
     obj.color = 'glorious gold'
-};paintIt(spaceship);   
+};
+paintIt(spaceship);   
 spaceship.color // Returns 'glorious gold'
 
+let spaceship2 = {
+    homePlanet : 'Earth',
+    color : 'red'
+};
+let tryReassignment = obj => {
+    obj = {
+      identified : false, 
+      'transport type' : 'flying'
+    }
+console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
+};
+  
+tryReassignment(spaceship2) // The attempt at reassignment does not work.
+spaceship2 // Still returns {homePlanet : 'Earth', color : 'red'};   
+spaceship2 = {
+    identified : false, 
+    'transport type': 'flying'
+}; // Regular reassignment still works.
+
+
+let spaceship3 = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+  };
+  // Write your code below
+  let greenEnergy = obj => {
+      obj['Fuel Type'] = 'avocado oil'
+  };
+  let remotelyDisable = obj => {
+      obj.disabled = true
+  };
+  greenEnergy(spaceship3);
+  remotelyDisable(spaceship3);
+  console.log(spaceship3);
+  
