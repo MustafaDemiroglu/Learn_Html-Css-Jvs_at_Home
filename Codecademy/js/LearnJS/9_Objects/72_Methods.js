18863548804c476fd21a557c181d6fb4fb5dfe0c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-18 22:36:48 +0100

    method

--- Codecademy/js/LearnJS/9_Objects/72_Methods.js
+++ Codecademy/js/LearnJS/9_Objects/72_Methods.js
@@ -9,6 +9,17 @@ const alienShip2 = {
       console.log('Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.')
     }
 };
-
 alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
 
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+// Write your code below
+const alienShip3 = {
+    retreat () { 
+      console.log('We no longer wish to conquer your planet. It is full of dogs, which we do not care for.')
+    },
+    takeOff() { 
+      console.log('Spim... Borp... Glix... Blastoff!')
+    }
+}; 
+alienShip3.retreat();
+alienShip3.takeOff();
