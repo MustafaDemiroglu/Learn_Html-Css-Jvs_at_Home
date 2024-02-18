4d33ee6f0a85ff4c9c663e2de109adf58c44b589
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-18 22:46:29 +0100

    nested Objekts

---
+++ Codecademy/js/LearnJS/9_Objects/73_NestedObjects.js
@@ -0,0 +1,65 @@
+const spaceship = {
+    telescope: {
+       yearBuilt: 2018,
+       model: '91031-XLT',
+       focalLength: 2032 
+    },
+   crew: {
+       captain: { 
+           name: 'Sandra', 
+           degree: 'Computer Engineering', 
+           encourageTeam() { console.log('We got this!') } 
+        }
+   },
+   engine: {
+       model: 'Nimbus2000'
+    },
+    nanoelectronics: {
+        computer: {
+           terabytes: 100,
+           monitors: 'HD'
+        },
+       'back-up': {
+          battery: 'Lithium',
+          terabytes: 50
+        }
+   }
+}; 
+spaceship.nanoelectronics['back-up'].battery; // Returns 'Lithium'
+
+let spaceship2 = {
+    passengers: null,
+    telescope: {
+      yearBuilt: 2018,
+      model: "91031-XLT",
+      focalLength: 2032 
+    },
+    crew: {
+      captain: { 
+        name: 'Sandra', 
+        degree: 'Computer Engineering', 
+        encourageTeam() { console.log('We got this!') },
+       'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+      model: "Nimbus2000"
+    },
+    nanoelectronics: {
+      computer: {
+        terabytes: 100,
+        monitors: "HD"
+      },
+      'back-up': {
+        battery: "Lithium",
+        terabytes: 50
+      }
+    }
+  }; 
+  
+  let capFave = spaceship2.crew.captain['favorite foods'][0];
+  spaceship2.passengers = [{name: 'Space Dog'}];
+  let firstPassenger = spaceship2.passengers[0];
+  
+  console.log(capFave);
+  console.log(firstPassenger);
+  
