f17ecebfb5b399c21fd83bb4c4675126ea0ad9ba
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-18 22:48:06 +0100

    df

---
+++ Codecademy/js/LearnJS/9_Objects/74_PassByReference.js
@@ -0,0 +1,9 @@
+const spaceship = {
+    homePlanet : 'Earth',
+    color : 'silver'
+}; 
+let paintIt = obj => {
+    obj.color = 'glorious gold'
+};paintIt(spaceship);   
+spaceship.color // Returns 'glorious gold'
+
