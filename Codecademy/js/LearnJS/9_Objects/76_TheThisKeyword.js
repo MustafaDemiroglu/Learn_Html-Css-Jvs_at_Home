43914c153cfb3af6723f4f7eb04af1d8619c1fc9
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-19 22:03:13 +0100

    this finished

--- Codecademy/js/LearnJS/9_Objects/76_TheThisKeyword.js
+++ Codecademy/js/LearnJS/9_Objects/76_TheThisKeyword.js
@@ -4,3 +4,37 @@ const goat = {
       console.log('baaa');
     }
 };
+goat.makeSound(); // Prints baaa
+
+const goat2 = {
+    dietType: 'herbivore',
+    makeSound() {
+      console.log('baaa');
+    },
+    diet() {
+      console.log(dietType);
+    }
+};
+goat2.diet(); 
+// Output will be "ReferenceError: dietType is not defined"
+
+const goat3 = {
+    dietType: 'herbivore',
+    makeSound() {
+      console.log('baaa');
+    },
+    diet() {
+      console.log(this.dietType);
+    }
+}; 
+goat3.diet(); 
+// Output: herbivore
+
+const robot = {
+    model : '1E78V2',
+    energyLevel : '100',
+    provideInfo() {
+        return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+      }
+};
+console.log(robot.provideInfo());
