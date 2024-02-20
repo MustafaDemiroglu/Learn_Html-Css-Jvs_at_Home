dcf37b387e9371646bc91825cca26bd983b74fe3
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-20 22:45:16 +0100

    seter3

--- Codecademy/js/LearnJS/9_Objects/80_Setters.js
+++ Codecademy/js/LearnJS/9_Objects/80_Setters.js
@@ -11,4 +11,7 @@ const person = {
 person.age = 40;
 console.log(person._age); // Logs: 40
 person.age = '40'; // Logs: You must assign a number to age
+person._age = 'forty-five'
+console.log(person._age); // Prints forty-five
+
 
