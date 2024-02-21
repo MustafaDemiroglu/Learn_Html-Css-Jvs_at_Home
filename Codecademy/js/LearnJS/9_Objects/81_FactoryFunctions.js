de17234d5f4c0dd5f5c168302892813387017a9b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-21 22:17:18 +0100

    beep boop

--- Codecademy/js/LearnJS/9_Objects/81_FactoryFunctions.js
+++ Codecademy/js/LearnJS/9_Objects/81_FactoryFunctions.js
@@ -11,3 +11,16 @@ const monsterFactory = (name, age, energySource, catchPhrase) => {
 
 const ghost = monsterFactory('Ghouly', 251, 'ectoplasm', 'BOO!');
 ghost.scare(); // 'BOO!'
+
+const robotFactory = (model, mobile) => {
+    return { 
+      model: model, 
+      mobile: mobile,
+      beep() {
+        console.log('Beep Boop');
+      } 
+    }
+};
+const tinCan = robotFactory('P-500', true);
+tinCan.beep();  // Beep Boop
+  
