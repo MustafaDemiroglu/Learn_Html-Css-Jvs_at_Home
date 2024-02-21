8a6677e40129534b201c3ce394190f5d6cd10249
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-21 22:24:16 +0100

    Destructured

---
+++ Codecademy/js/LearnJS/9_Objects/83_DestructuredAssignment.js
@@ -0,0 +1,34 @@
+const vampire = {
+    name: 'Dracula',
+    residence: 'Transylvania',
+    preferences: {
+      day: 'stay inside',
+      night: 'satisfy appetite'
+    }
+};
+
+const residence2 = vampire.residence; 
+console.log(residence2); // Prints 'Transylvania' 
+
+const {residence} = vampire; 
+console.log(residence); // Prints 'Transylvania'
+
+const { day } = vampire.preferences; 
+console.log(day); // Prints 'stay inside'
+
+
+
+const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+};
+
+
