51beaee4532beb62a7957f1924738e01f52d96bb
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-22 22:36:05 +0100

    go on

--- Codecademy/js/LearnJS/Projekt_MealMaker/MealMaker.js
+++ Codecademy/js/LearnJS/Projekt_MealMaker/MealMaker.js
@@ -1,5 +1,21 @@
 const menu = {
     _meal :'',
-    _price : 0
-  
-  };
+    _price : 0,
+    set meal(mealToCheck){
+        if (typeof mealToCheck === 'string'){
+          this._meal = mealToCheck;
+        } else {
+          console.log('You must assign a food to meal');
+        }
+    }, 
+    set price (priceToCheck){
+        if (typeof priceToCheck === 'number'){
+          this._price = priceToCheck;
+        } else {
+          console.log('You must assign a number to price');
+        }
+    } 
+};
+menu.meal = 80;
+menu.price = 'kebap';
+console.log(menu);
