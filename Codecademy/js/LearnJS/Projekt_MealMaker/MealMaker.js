65b4e69cb20ca13b522eaa354bae680d2f8eeb0d
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-22 22:44:46 +0100

    finished

--- Codecademy/js/LearnJS/Projekt_MealMaker/MealMaker.js
+++ Codecademy/js/LearnJS/Projekt_MealMaker/MealMaker.js
@@ -14,8 +14,44 @@ const menu = {
         } else {
           console.log('You must assign a number to price');
         }
-    } 
-};
-menu.meal = 80;
-menu.price = 'kebap';
+    },
+    get todaysSpecial() {
+        if (this._meal && this._price){
+          return `Today's Special is ${this._meal} for $${this._price}!`;
+        } else {
+          return 'Meal or price was not set correctly!.';
+        }
+      } 
+  };
+  menu.meal = 80;
+  menu.price = 'kebap';
+  console.log(menu);
+  console.log(menu.todaysSpecial);
+
+/*
+You must assign a food to meal
+You must assign a number to price
+{
+  _meal: '',
+  _price: 0,
+  meal: [Setter],
+  price: [Setter],
+  todaysSpecial: [Getter]
+}
+Meal or price was not set correctly!.
+*/
+
+menu.meal = 'Adana Kebap';
+menu.price = 30;
 console.log(menu);
+console.log(menu.todaysSpecial);
+/* 
+{
+  _meal: 'Adana Kebap',
+  _price: 30,
+  meal: [Setter],
+  price: [Setter],
+  todaysSpecial: [Getter]
+}
+Today's Special is Adana Kebap for $30!
+*/
