a58d545e093392c4e4323e857887110610cfcebd
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-24 00:00:30 +0100

    correction

--- Codecademy/js/Projekt_MealMaker/MealMaker.js
+++ Codecademy/js/Projekt_MealMaker/MealMaker.js
@@ -1,27 +1,57 @@
-const team = {
-  _players: [
-    {firstName: 'John', lastName: 'Doe', age: 25},
-    {firstName: 'Jane', lastName: 'Smith', age: 28},
-    {firstName: 'Michael', lastName: 'Johnson', age: 30}
-  ],
-  _games: [
-    {opponent: 'Team A', teamPoints: 20, opponentPoints: 15},
-    {opponent: 'Team B', teamPoints: 18, opponentPoints: 22},
-    {opponent: 'Team C', teamPoints: 25, opponentPoints: 20}
-  ],
-  get players() {
-       return this._players;
-  },
-  get games() {
-       return this._games;
-  },
-  addPlayer(newFirstName, newLastName, newAge) {
-    const newPlayer = {
-      firstName: newFirstName,
-      lastName: newLastName,
-      age: newAge
-    };
-    this._players.push(newPlayer);
-  }
+const menu = {
+    _meal :'',
+    _price : 0,
+    set meal(mealToCheck){
+        if (typeof mealToCheck === 'string'){
+          this._meal = mealToCheck;
+        } else {
+          console.log('You must assign a food to meal');
+        }
+    }, 
+    set price (priceToCheck){
+        if (typeof priceToCheck === 'number'){
+          this._price = priceToCheck;
+        } else {
+          console.log('You must assign a number to price');
+        }
+    },
+    get todaysSpecial() {
+        if (this._meal && this._price){
+          return `Today's Special is ${this._meal} for $${this._price}!`;
+        } else {
+          return 'Meal or price was not set correctly!.';
+        }
+      } 
+  };
+  menu.meal = 80;
+  menu.price = 'kebap';
+  console.log(menu);
+  console.log(menu.todaysSpecial);
 
-};
+/*
+You must assign a food to meal
+You must assign a number to price
+{
+  _meal: '',
+  _price: 0,
+  meal: [Setter],
+  price: [Setter],
+  todaysSpecial: [Getter]
+}
+Meal or price was not set correctly!.
+*/
+
+menu.meal = 'Adana Kebap';
+menu.price = 30;
+console.log(menu);
+console.log(menu.todaysSpecial);
+/* 
+{
+  _meal: 'Adana Kebap',
+  _price: 30,
+  meal: [Setter],
+  price: [Setter],
+  todaysSpecial: [Getter]
+}
+Today's Special is Adana Kebap for $30!
+*/
