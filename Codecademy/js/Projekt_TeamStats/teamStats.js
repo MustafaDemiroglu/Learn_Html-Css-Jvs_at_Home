96977b318c63e3918069eec1478c3270b2c0dbdc
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-24 00:00:57 +0100

    hgfdss

--- Codecademy/js/Projekt_TeamStats/teamStats.js
+++ Codecademy/js/Projekt_TeamStats/teamStats.js
@@ -8,5 +8,20 @@ const team = {
       {opponent: 'Team A', teamPoints: 20, opponentPoints: 15},
       {opponent: 'Team B', teamPoints: 18, opponentPoints: 22},
       {opponent: 'Team C', teamPoints: 25, opponentPoints: 20}
-    ]
+    ],
+    get players() {
+         return this._players;
+    },
+    get games() {
+         return this._games;
+    },
+    addPlayer(newFirstName, newLastName, newAge) {
+      const newPlayer = {
+        firstName: newFirstName,
+        lastName: newLastName,
+        age: newAge
+      };
+      this._players.push(newPlayer);
+    }
+  
 };
