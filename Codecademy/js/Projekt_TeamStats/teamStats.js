8462567c26873e74cb3368b0946c65ffc020ecff
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-23 23:54:15 +0100

    teamstats

---
+++ Codecademy/js/Projekt_TeamStats/teamStats.js
@@ -0,0 +1,12 @@
+const team = {
+    _players: [
+      {firstName: 'John', lastName: 'Doe', age: 25},
+      {firstName: 'Jane', lastName: 'Smith', age: 28},
+      {firstName: 'Michael', lastName: 'Johnson', age: 30}
+    ],
+    _games: [
+      {opponent: 'Team A', teamPoints: 20, opponentPoints: 15},
+      {opponent: 'Team B', teamPoints: 18, opponentPoints: 22},
+      {opponent: 'Team C', teamPoints: 25, opponentPoints: 20}
+    ]
+};
