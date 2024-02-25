702f4672e8d90372f2f01a2e5ca17fc2739d11e9
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-25 23:14:11 +0100

    Construction an error

---
+++ Codecademy/js/LearnJS/99_DebuggingJavaScript/7_ConstructingAnError.js
@@ -0,0 +1,10 @@
+console.log(Error('Your password is too weak.'));
+// Prints: Error: Your password is too weak.
+console.log(new Error('Your password is too weak.'));
+// Prints: Error: Your password is too weak.
+
+// Write your code below:
+console.log(Error('User missing name'));
+
+
+console.log('Will logging the error stop our program from running?');
