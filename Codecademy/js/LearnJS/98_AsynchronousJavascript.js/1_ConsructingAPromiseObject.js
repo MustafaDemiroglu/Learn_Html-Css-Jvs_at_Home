2493aab198413cac45efbea5f5091847500a8c6a
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-26 22:21:15 +0100

    promise

--- Codecademy/js/LearnJS/98_AsynchronousJavascript.js/1_ConsructingAPromiseObject.js
+++ Codecademy/js/LearnJS/98_AsynchronousJavascript.js/1_ConsructingAPromiseObject.js
@@ -1,3 +1,12 @@
 const executorFunction = (resolve, reject) => { };
 const myFirstPromise = new Promise(executorFunction);
 
+const executorFunction2 = (resolve, reject) => {
+    if (someCondition) {
+        resolve('I resolved!');
+    } else {
+        reject('I rejected!'); 
+    }
+   }
+   const myFirstPromise2 = new Promise(executorFunction2);
+   
