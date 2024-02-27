fe2cc9dddd22876174db4a60d399953306867f77
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-27 21:15:11 +0100

    ascycronos

--- Codecademy/js/LearnJS/98_AsynchronousJavascript.js/2_TheNodesetTimeOut.js
+++ Codecademy/js/LearnJS/98_AsynchronousJavascript.js/2_TheNodesetTimeOut.js
@@ -2,4 +2,13 @@ const delayedHello = () => {
     console.log('Hi! This is an asynchronous greeting!');
   };
   
-  setTimeout(delayedHello, 2000);
+setTimeout(delayedHello, 2000);
+
+const returnPromiseFunction = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(( ) => {resolve('I resolved!')}, 1000);
+    });
+};
+  
+const prom = returnPromiseFunction();
+  
