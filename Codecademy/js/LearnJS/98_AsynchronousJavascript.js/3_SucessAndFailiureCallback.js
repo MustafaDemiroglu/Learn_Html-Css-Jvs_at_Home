3cb3a498707b3dd455083eb6ba6ca5abb96bb274
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-29 11:42:50 +0100

    finished

--- Codecademy/js/LearnJS/98_AsynchronousJavascript.js/3_SucessAndFailiureCallback.js
+++ Codecademy/js/LearnJS/98_AsynchronousJavascript.js/3_SucessAndFailiureCallback.js
@@ -21,3 +21,18 @@ const handleFailure = (rejectionReason) => {
     console.log(rejectionReason);
 };
 prom.then(handleSuccess, handleFailure);
+
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+// Write your code below:
+const handleSuccess3 = (resolvedValue) => {
+    console.log(resolvedValue);
+};
+
+const handleFailure3 = (rejectReason) => {
+    console.log(rejectReason);
+};
+
+prom.checkInventory(order).then(handleSuccess3, handleFailure3);
