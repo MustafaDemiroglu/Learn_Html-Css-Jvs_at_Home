cd52a0c30746004f5a92dc4e8ad365305853f6ff
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-29 21:43:03 +0100

    chaining multiple promises

---
+++ Codecademy/js/LearnJS/98_AsynchronousJavascript.js/5_ChainingMultiplePromises.js
@@ -0,0 +1,31 @@
+firstPromiseFunction()
+.then((firstResolveVal) => {
+  return secondPromiseFunction(firstResolveVal);
+})
+.then((secondResolveVal) => {
+  console.log(secondResolveVal);
+});
+
+
+const {checkInventory, processPayment, shipOrder} = require('./library2.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+checkInventory(order)
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+ 
+})
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+  
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});
