5e3f9a927810f4a34a354b8c8706c5ed4ad088c0
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-29 10:54:18 +0100

    update

--- Codecademy/js/LearnJS/98_AsynchronousJavascript.js/3_SucessAndFailiureCallback.js
+++ Codecademy/js/LearnJS/98_AsynchronousJavascript.js/3_SucessAndFailiureCallback.js
@@ -1,8 +1,23 @@
-const prom = new Promise((resolve, reject) => {
+const prom2 = new Promise((resolve, reject) => {
     resolve('Yay!');
   });  
-const handleSuccess = (resolvedValue) => {
+const handleSuccess2 = (resolvedValue) => {
     console.log(resolvedValue);
 };  
 prom.then(handleSuccess); // Prints: 'Yay!'
 
+let prom = new Promise((resolve, reject) => {
+    let num = Math.random();
+    if (num < .5 ){
+      resolve('Yay!');
+    } else {
+      reject('Ohhh noooo!');
+    }
+});
+const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+};
+const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+};
+prom.then(handleSuccess, handleFailure);
